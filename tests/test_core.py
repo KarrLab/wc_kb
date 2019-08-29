a86bede2c70d24f0d20cefc2805934f1f3cfa279
Yin Hoon Chew <cyinhoon@yahoo.com>
2019-08-29 17:42:38 -0400

    Delete debug print statement that might be the cause of abortion

--- tests/test_core.py
+++ tests/test_core.py
@@ -508,7 +508,6 @@ class MetaboliteSpeciesTypeTestCase(unittest.TestCase):
                 '/s1'))
         met = core.MetaboliteSpeciesType(properties = [speciesTypeProperties])
 
-        print(met.get_structure())
         self.assertEqual(met.get_structure(), speciesTypeProperties.value)
         self.assertEqual(met.get_empirical_formula(),
                          chem.EmpiricalFormula('C10H12N5O7P'))
