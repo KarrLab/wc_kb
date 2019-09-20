f987a827f43587de095100bcf7b5d6ff7ede727c
Jonathan Karr <jonrkarr@gmail.com>
2019-09-20 11:57:54 -0400

    updating list of files that need to be migrated

--- migrations/run_custom_migration.sh
+++ migrations/run_custom_migration.sh
@@ -8,12 +8,11 @@ ${cmd} eu ~/Documents/wc_kb/tests/fixtures/eukaryote_core.xlsx
 # h1_hesc
 ${cmd} eu ~/Documents/h1_hesc/h1_hesc/kb_gen/core.xlsx
 ${cmd} eu ~/Documents/h1_hesc/tests/code/fixtures/eukaryote_core.xlsx
-${cmd} eu ~/Documents/h1_hesc/tests/code/fixtures/eukaryote_model.xlsx
 
 # mycoplasma_pneumoniae
 ${cmd} pro ~/Documents/mycoplasma_pneumoniae/mycoplasma_pneumoniae/kb/mycoplasma_pneumoniae_kb.xlsx
 ${cmd} pro ~/Documents/mycoplasma_pneumoniae/mycoplasma_pneumoniae/kb/mycoplasma_pneumoniae_kb_old_version.xlsx
-${cmd} pro ~/Documents/mycoplasma_pneumoniae/mycoplasma_pneumoniae/network_model/mycoplasma_pneumoniae_kb_wFunctions
+${cmd} pro ~/Documents/mycoplasma_pneumoniae/mycoplasma_pneumoniae/network\ model/mycoplasma_pneumoniae_kb_wFunctions.xlsx
 
 # wc_model_gen
 ${cmd} pro ~/Documents/wc_model_gen/tests/fixtures/min_model_kb.xlsx
