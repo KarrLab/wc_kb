36481653ab937a97faf3f2bcd1c54c87de99b35c
Jonathan Karr <jonrkarr@gmail.com>
2020-04-27 00:37:33 -0400

    updating for changes to obj_tables

--- migrations/run_custom_migration.py
+++ migrations/run_custom_migration.py
@@ -1,22 +1,22 @@
 """ Migration of WC-KB-encoded files
 
 :Author: Jonathan Karr <karr@mssm.edu>
-:Date: 2019-10-10
-:Copyright: 2019, Karr Lab
+:Date: 2020-04-27
+:Copyright: 2020, Karr Lab
 :License: MIT
 """
 
 import os.path
 import sys
 sys.path.insert(0, 'migrations')
-import migration_2020_03_09 as migration
+import migration_2020_04_27 as migration
 
 base_dir = os.path.expanduser('~/Documents')
 
 paths = [
     # wc_kb
     {'taxon': 'pro', 'path': 'wc_kb/tests/fixtures/prokaryote_core.xlsx'},
-    {'taxon': 'eu', 'path': 'wc_kb/tests/fixtures/eukaryote_core.xlsx'},    
+    {'taxon': 'eu', 'path': 'wc_kb/tests/fixtures/eukaryote_core.xlsx'},
 
     # h1_hesc
     {'taxon': 'eu', 'path': 'h1_hesc/h1_hesc/kb_gen/core.xlsx'},
